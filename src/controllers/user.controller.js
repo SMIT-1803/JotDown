610da6e4d6538659dc7001f2c29de5daac87a49a
SMIT-1803 <smitsachin1@gmail.com>
2025-08-22 18:00:21 -0700

    Register and Login user controllers added

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,138 @@
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utiils/ApiError.js";
+import { ApiResponse } from "../utiils/ApiResponse.js";
+import jwt from "jsonwebtoken";
+
+async function generateAccessAndRefreshTokens(userId) {
+  try {
+    const user = await User.findById(user._id);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {}
+}
+
+async function registerUser(req, res) {
+  /*
+Get user information from front end
+Check if anything required is passed empty or not correctly formatted
+Check if the user already exists or not
+If user does not exist create a new user object and create new entry in the database
+Remove password and refresh token from the response received from MongoDB on entry.
+Check if the entry is created or not
+return response
+
+*/
+  // Getting user data
+  const { username, email, fullName, password } = req.body;
+
+  // Validation
+  {
+    [username, email, fullName, password].map((item) => {
+      if (item == "") {
+        throw new ApiError(400, "One of the input fields are empty!!");
+      }
+    });
+  }
+
+  if (!email.includes("@") || !email !== email.toLowerCase()) {
+    throw new ApiError(400, "Please enter a valid email");
+  }
+
+  // Check if user exists
+
+  try {
+    const userExist = await User.findOne({
+      $or: [{ username }, { email }],
+    });
+
+    if (userExist) {
+      throw new ApiError(400, "User already exists, please login");
+    }
+
+    const user = await User.create({
+      fullName,
+      username: username.toLowerCase(),
+      password,
+      email,
+    });
+    const createdUser = await User.findById(user._id).select(
+      "-password -refreshToken"
+    );
+    if (!createdUser) {
+      throw new ApiError(500, "User not created and registered");
+    }
+
+    return res
+      .status(201)
+      .json(new ApiResponse(200, createdUser, "User registered successfully"));
+  } catch (error) {
+    throw new ApiError(400, "Something went wrong while registering user.");
+  }
+}
+
+async function loginUser(req, res) {
+  const { email, password } = req.body;
+  {
+    [email, password].map((item) => {
+      if (item == "") {
+        throw new ApiError(400, "One of the input fields are empty!!");
+      }
+    });
+  }
+
+  if (!email.includes("@") || email !== email.toLowerCase()) {
+    throw new ApiError(400, "Please enter a valid email");
+  }
+
+  try {
+    const user = await User.findOne({
+      $or: [{ email }],
+    });
+    if (!userExist) {
+      throw new ApiError(400, "User not found. Please register");
+    }
+
+    const isPasswordCorrect = await user.isPasswordCorrect(password);
+    if (!isPasswordCorrect) {
+      throw new ApiError(401, "Invalid user credentials");
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+      user._id
+    );
+
+    const loggedInUser = await User.findById(user._id).select(
+      "-password -refreshToken"
+    );
+
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", refreshToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          {
+            user: loggedInUser,
+            accessToken,
+            refreshToken,
+          },
+          "User logged in successfully"
+        )
+      );
+  } catch (error) {
+    throw new ApiError(400, "Something went wrong while logging user in.");
+  }
+}
+
+export { registerUser, loginUser };
