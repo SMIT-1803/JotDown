e046e344a66a26798f713f14c9e557aef91aa3f5
SMIT-1803 <smitsachin1@gmail.com>
2025-08-22 18:01:17 -0700

    Register and Login routes added

--- src/app.js
+++ src/app.js
@@ -16,4 +16,11 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+
+// routes import
+ import userRoutes from "./routes/user.routes.js"
+
+ //routes declaration
+app.use("/users",userRoutes)
+
 export { app };
