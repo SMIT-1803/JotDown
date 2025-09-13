6d7122b455d604f0fc5ed8f9595085f18e3aef5d
SMIT-1803 <smitsachin1@gmail.com>
2025-09-13 11:25:42 -0700

    Created client.js to create api instance and auth.js for authentication methods

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import TestAuth from "./test/TestAuth.jsx"
 
 function App() {
 
   return (
     <>
+    <TestAuth/>
     </>
   )
 }
